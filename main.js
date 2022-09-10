5e9fab39342fff1913d585b6282da72795cf541b
Rustam7717 <dj_rustam85@mail.ru>
2022-09-10 13:21:42 +0600

    Fizzzyyyy

---
+++ main.js
@@ -0,0 +1,55 @@
+///////////////////  1 TASK    ////////////////////
+// let star = "*";
+// for (let i = 1; i < 8; i++){
+//     if(i != 8){
+//         console.log(star);
+//         star+="*"
+//     }
+// }
+
+///////////////////////// 2 TASK  ////////////////////////
+// const fizz = "Fizz";
+// const buzz = "Buzz";
+// for (let i = 1; i<=100; i++) 
+//     if (i%15 == 0){
+//         console.log('fizz+buzz'); 
+      
+//     } else if(i%3 == 0){
+//         console.log('fizz');
+
+//     }else if(i%5 == 0){
+//         console.log('buzz')
+
+//     }else{
+//         console.log(i);
+//     }
+
+//////////////////// 3 TASK //////////////////////////
+
+
+// function chess(a, b) {
+//     var toleft = "# ";
+//     var torigth = " #";
+//     var odd = "";
+//     var even = "";
+//     for (var i = 0; i < a / 2; i += 1) {
+//       odd += torigth;
+//       even += toleft;
+//     }
+  
+//     for (var j = 0; j < b; j += 1) {
+//       if (j % 2 != 0) {
+//         console.log(odd);
+//       } else {
+//         console.log(even);
+//       }
+//     }
+//   }
+  
+//   chess(8 ,8);
+
+
+
+
+
+
